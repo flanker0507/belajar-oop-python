0dc69e3f7f1da4713352f0cb7f17ef2f1c127b82
flanker0507 <yudawirapratama05@gmail.com>
2024-11-23 14:40:45 +0700

    Latihan Inheritance

---
+++ latihanOOP3.py
@@ -0,0 +1,14 @@
+from Hero import HeroInteligence,HeroStrength
+
+lina = HeroInteligence('lina')
+slardar = HeroStrength('slardar')
+
+lina.show_info()
+slardar.show_info()
+
+lina.gainExp = 200
+slardar.gainExp = 250
+
+lina.show_info()
+slardar.show_info()
+
