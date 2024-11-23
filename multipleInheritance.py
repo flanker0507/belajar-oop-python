e6d93afd5fbb566efd0f0a086546c6ed23cf9d5f
flanker0507 <yudawirapratama05@gmail.com>
2024-11-23 17:29:31 +0700

    Multiple Inheritance

---
+++ multipleInheritance.py
@@ -0,0 +1,30 @@
+class Team():
+    def setTeam(self,team):
+        self.team = team
+
+    def showTeam(self):
+        print(self.team)
+
+class Tipe_Hero():
+    def setTipe(self,tipe):
+        self.tipe = tipe
+
+    def showTipe(self):
+        print(self.tipe)
+
+class Hero(Team,Tipe_Hero):
+    def __init__(self,name,health):
+        self.name = name
+        self.health = health
+
+    def showHero(self):
+        print(self.name, self.health)
+
+
+Ucup = Hero('Ucup',100)
+Ucup.setTeam("Pink")
+Ucup.setTipe("Cundang")
+
+Ucup.showTipe()
+Ucup.showTeam()
+Ucup.showHero()
